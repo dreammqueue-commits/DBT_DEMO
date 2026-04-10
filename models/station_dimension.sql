4e7718983897b86e177c3d6e8feb98e6e03bd099
dreammqueue@gmail.com <dreammqueue@gmail.com>
2026-04-10 16:23:12 +0000

    correction of the data to match the year 2018

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -1,7 +1,7 @@
 WITH bike AS (
     SELECT  DISTINCT start_statio_ID AS station_ID, start_station_name AS station_name, start_lat AS station_lat, start_lng AS station_lng
-    FROM    {{source('demo', 'bike')}}
-    WHERE   ride_id != 'ride_id'
+    FROM    {{ ref('stg_bike') }}
+    WHERE   ride_id != 'bikeid'
     
 )
 
--- models/trip_fact.sql
+++ models/trip_fact.sql
@@ -1,14 +1,14 @@
 WITH trips AS (
     SELECT  RIDE_ID, 
-            RIDEABLE_TYPE, 
+            -- RIDEABLE_TYPE, 
             DATE(TO_TIMESTAMP(STARTED_AT)) AS TRIP_DATE, 
             START_STATIO_ID AS START_STATION_ID, 
             END_STATION_ID,
             MEMBER_CSUAL AS MEMBER_CASUAL,
             TIMESTAMPDIFF(SECOND, STARTED_AT, ENDED_AT) AS TRIP_DURATION_SECONDS
-    FROM    {{source('demo', 'bike')}}
+    FROM    {{ ref('stg_bike') }}
     WHERE   ride_id != 'ride_id'
-    LIMIT 10
+   
 )
 
 SELECT *
